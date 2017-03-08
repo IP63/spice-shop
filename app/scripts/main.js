90734ee6edfdefd6bcb2957523f3cf3cc41502fa
Dawid Wojnowski <dawid.wojnowski@outlook.com>
2017-03-08 03:27:11 +0100

    Added scrolling to section

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,27 +1,29 @@
-// WOW plugin
-new WOW().init();
-
-// select2 plugin
-$('select').select2({
-  width: '150px',
-  minimumResultsForSearch: Infinity
-});
-
-// Bootstrap tooltips
-$(function () {
-  $('[data-toggle="tooltip"]').tooltip();
-});
-
-
-
-
 (function(window) {
-
   'use strict';
 
-  /**
-   * Extend Object helper function.
-   */
+  // WOW plugin
+  new WOW().init();
+
+  // select2 plugin
+  $('select').select2({
+    width: '150px',
+    minimumResultsForSearch: Infinity
+  });
+
+  // Bootstrap tooltips
+  $(function () {
+    $('[data-toggle="tooltip"]').tooltip();
+  });
+
+  // scroll to sections
+  $('.main-nav, .slider, .offer').on('click', 'a', function(event){
+      event.preventDefault();
+      $('html, body').animate({
+          scrollTop: $( $.attr(this, 'href') ).offset().top
+      }, 500);
+  });
+
+  // Menu: Extend Object helper function
   function extend(a, b) {
     for(var key in b) {
       if(b.hasOwnProperty(key)) {
@@ -30,39 +32,27 @@ $(function () {
     }
     return a;
   }
-
-  /**
-   * Each helper function.
-   */
+  // Menu: Each helper function
   function each(collection, callback) {
     for (var i = 0; i < collection.length; i++) {
       var item = collection[i];
       callback(item);
     }
   }
-
-  /**
-   * Menu Constructor.
-   */
+  // Menu: Menu Constructor
   function Menu(options) {
     this.options = extend({}, this.options);
     extend(this.options, options);
     this._init();
   }
-
-  /**
-   * Menu Options.
-   */
+  // Menu: Menu Options
   Menu.prototype.options = {
     wrapper: '#o-wrapper',          // The content wrapper
     type: 'slide-left',             // The menu type
     menuOpenerClass: '.c-button',   // The menu opener class names (i.e. the buttons)
     maskId: '#c-mask'               // The ID of the mask
   };
-
-  /**
-   * Initialise Menu.
-   */
+  // Menu: Initialise Menu
   Menu.prototype._init = function() {
     this.body = document.body;
     this.wrapper = document.querySelector(this.options.wrapper);
@@ -72,27 +62,21 @@ $(function () {
     this.menuOpeners = document.querySelectorAll(this.options.menuOpenerClass);
     this._initEvents();
   };
-
-  /**
-   * Initialise Menu Events.
-   */
+  // Menu: Initialise Menu Events
   Menu.prototype._initEvents = function() {
-    // Event for clicks on the close button inside the menu.
+    // Event for clicks on the close button inside the menu
     this.closeBtn.addEventListener('click', function(e) {
       e.preventDefault();
       this.close();
     }.bind(this));
 
-    // Event for clicks on the mask.
+    // Event for clicks on the mask
     this.mask.addEventListener('click', function(e) {
       e.preventDefault();
       this.close();
     }.bind(this));
   };
-
-  /**
-   * Open Menu.
-   */
+  // Menu: Open Menu
   Menu.prototype.open = function() {
     this.body.classList.add('has-active-menu');
     this.wrapper.classList.add('has-' + this.options.type);
@@ -100,10 +84,7 @@ $(function () {
     this.mask.classList.add('is-active');
     this.disableMenuOpeners();
   };
-
-  /**
-   * Close Menu.
-   */
+  // Menu: Close menu
   Menu.prototype.close = function() {
     this.body.classList.remove('has-active-menu');
     this.wrapper.classList.remove('has-' + this.options.type);
@@ -111,28 +92,19 @@ $(function () {
     this.mask.classList.remove('is-active');
     this.enableMenuOpeners();
   };
-
-  /**
-   * Disable Menu Openers.
-   */
+  // Menu: Disable Menu Openers
   Menu.prototype.disableMenuOpeners = function() {
     each(this.menuOpeners, function(item) {
       item.disabled = true;
     });
   };
-
-  /**
-   * Enable Menu Openers.
-   */
+  // Menu: Enable Menu Openers
   Menu.prototype.enableMenuOpeners = function() {
     each(this.menuOpeners, function(item) {
       item.disabled = false;
     });
   };
-
-  /**
-   * Add to global namespace.
-   */
+  // Menu: Add to global namespace
   window.Menu = Menu;
 
 })(window);
